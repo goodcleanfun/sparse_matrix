465f5e5deb92a2ff28e715d6b13a56300e53dba7
Al <albarrentine@gmail.com>
2023-06-17 21:55:15 -0400

    [double] adding double_sparse_matrix

---
+++ src/double_sparse_matrix.h
@@ -0,0 +1,9 @@
+#ifndef DOUBLE_SPARSE_MATRIX_H
+#define DOUBLE_SPARSE_MATRIX_H
+
+#include "sparse_matrix.h"
+#include "num_arrays/double_array.h"
+
+SPARSE_MATRIX_INIT(double_sparse_matrix, double, double_array)
+
+#endif
