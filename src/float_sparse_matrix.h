9cdefbfcf8718e8c83462472237f97144df1d86a
Al <albarrentine@gmail.com>
2023-06-17 21:55:33 -0400

    [float] adding float_sparse_matrix

---
+++ src/float_sparse_matrix.h
@@ -0,0 +1,9 @@
+#ifndef FLOAT_SPARSE_MATRIX_H
+#define FLOAT_SPARSE_MATRIX_H
+
+#include "sparse_matrix.h"
+#include "num_arrays/float_array.h"
+
+SPARSE_MATRIX_INIT(float_sparse_matrix, float, float_array)
+
+#endif
