172a744272d0a1a7639452fffd7f47c90e0cd83d
Al <albarrentine@gmail.com>
2023-07-26 13:49:31 -0400

    [rm] removing old file

--- src/float_sparse_row_matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLOAT_SPARSE_MATRIX_H
-#define FLOAT_SPARSE_MATRIX_H
-
-#include "sparse_matrix.h"
-#include "num_arrays/float_array.h"
-
-SPARSE_MATRIX_INIT(float_sparse_row_matrix, float, float_array, row, ROW)
-
-#endif
