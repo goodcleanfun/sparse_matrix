2ab10c8bef826823640df16fb5363d9f1a19cd3c
Al <albarrentine@gmail.com>
2024-01-28 03:10:17 -0500

    [test] adding more tests of matrix-vector and matrix-matrix products

--- test.c
+++ test.c
@@ -6,11 +6,11 @@
 #include "greatest/greatest.h"
 #include "float_csr.h"
 
-TEST test_float_csr_matrix(void) {
-    float_csr_matrix *sp = float_csr_matrix_new();
+TEST test_float_csr(void) {
+    float_csr *sp = float_csr_new();
     uint32_t indices_1[] = {0, 1, 2, 3};
     float values_1[] = {1.0, 1.0, 1.0, 1.0};
-    float_csr_matrix_append_row(sp, (uint32_t *)indices_1, (float *)values_1, sizeof(indices_1) / sizeof(uint32_t));
+    float_csr_append_row(sp, (uint32_t *)indices_1, (float *)values_1, sizeof(indices_1) / sizeof(uint32_t));
 
     ASSERT_EQ(sp->m, 1);
     ASSERT_EQ(sp->n, 4);
@@ -21,9 +21,9 @@ TEST test_float_csr_matrix(void) {
     }
 
     uint32_t indices_2[] = {1, 4, 5};
-    float values_2[] = {1.0, 2.0, 1.0};
+    float values_2[] = {1.0, 1.0, 1.0};
 
-    float_csr_matrix_append_row(sp, indices_2, values_2, sizeof(indices_2) / sizeof(uint32_t));
+    float_csr_append_row(sp, indices_2, values_2, sizeof(indices_2) / sizeof(uint32_t));
 
     ASSERT_EQ(sp->m, 2);
     ASSERT_EQ(sp->n, 6);
@@ -34,20 +34,60 @@ TEST test_float_csr_matrix(void) {
         ASSERT_EQ(sp->data->a[i], values_2[i - 4]);
     }
 
-    float_csr_matrix_append(sp, 4, 1.0);
-    float_csr_matrix_append(sp, 5, 1.0);
-    float_csr_matrix_append(sp, 6, 1.0);
-    float_csr_matrix_append(sp, 7, 1.0);
+    float_csr_append(sp, 4, 1.0);
+    float_csr_append(sp, 5, 1.0);
+    float_csr_append(sp, 6, 1.0);
+    float_csr_append(sp, 7, 1.0);
 
-    float_csr_matrix_finalize_row(sp);
+    float_csr_finalize_row(sp);
 
     ASSERT_EQ(sp->m, 3);
+    ASSERT_EQ(sp->n, 8);
 
-    float_csr_matrix_destroy(sp);
+    float_array *vec = float_array_new_size(sp->n);
+    for (size_t i = 0; i < sp->n; i++) {
+        float_array_push(vec, 1.0);
+    }
+
+    float_array *res = float_array_new_size(sp->m);
+    float_csr_dot_vector(sp, vec->a, vec->n, res->a, sp->m);
+    uint32_t row = 0, row_start = 0, row_len = 0;
+    compressed_sparse_matrix_foreach(sp, row, row_start, row_len, {
+        ASSERT_EQ(res->a[row], row_len * 1.0);
+    })
+
+    size_t num_rows = 2;
+    uint32_t rows[] = {0, 2};
+    float sparse_result[] = {0.0, 0.0};
+    ASSERT(float_csr_rows_dot_vector(sp, rows, num_rows, vec->a, vec->n, sparse_result, num_rows));
+    for (size_t i = 0; i < num_rows; i++) {
+        uint32_t row = rows[i];
+        uint32_t row_len = float_csr_len_row(sp, row);
+        ASSERT_EQ(sparse_result[i], row_len * 1.0);
+    }
+
+    float_csr *sp2 = float_csr_new();
+
+    float_csr_append_row(sp2, (uint32_t[]){0}, (float[]){3.0}, 1);
+    float_csr_append_row(sp2, (uint32_t[]){1}, (float[]){5.0}, 1);
+    float_csr_append_row(sp2, (uint32_t[]){1}, (float[]){2.0}, 1);
+    float_csr_append_row(sp2, (uint32_t[]){0, 1},(float[]) {3.0, 5.0}, 2);
+    float_csr_append_row(sp2, (uint32_t[]){1}, (float[]){8.0}, 1);
+    float_csr_append_row(sp2, (uint32_t[]){0}, (float[]){4.0}, 1);
+    float_csr_append_row(sp2, (uint32_t[]){0, 1}, (float[]){1.0, 2.0}, 2);
+    float_csr_append_row(sp2, (uint32_t[]){1}, (float[]){7.0}, 1);
+
+    float_csr *sp_prod_sp2 = float_csr_new();
+
+    ASSERT(float_csr_dot_sparse(sp, sp2, sp_prod_sp2, false));
+
+
+    float_array_destroy(vec);
+    float_array_destroy(res);
+    float_csr_destroy(sp);
 
     PASS();
 }
-
 
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
@@ -55,7 +95,7 @@ GREATEST_MAIN_DEFS();
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
 
-    RUN_TEST(test_float_csr_matrix);
+    RUN_TEST(test_float_csr);
 
     GREATEST_MAIN_END();        /* display results */
 }
